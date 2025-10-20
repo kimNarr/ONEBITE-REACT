b4ebddbc285f3ca9a4534fa2e7a715c06c1553b1
dj_kim <ignitionhero@gmail.com>
2025-10-20 15:42:36 +0900

    firt commit

---
+++ section01/chapter05.js
@@ -0,0 +1,36 @@
+// 1. Number Type
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+
+// console.log(1 * "nan");
+
+// 2. String Type
+let myName = "이정환";
+let myLocation = "목동";
+let introduce = myName + myLocation;
+
+let introduceText = `${myName}은 ${myLocation}에 거주합니다`;
+
+
+// 3. Boolean Type
+let isSwitchOn = false;
+let isEmpty = true;
+
+// 4. null Type (아무것도 없다)
+let empty = null;
+
+// 5. undefined
+let none;
+console.log(none);
