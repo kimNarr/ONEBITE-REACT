c11a8f53c390bfcc0601e38c6a40c861e0a26422
dj_kim <ignitionhero@gmail.com>
2025-10-21 13:37:04 +0900

    feat: 함수 추가

--- section01/chapter11.js
+++ section01/chapter11.js
@@ -7,12 +7,20 @@
 // greeting();
 // console.log("호출후");
 
+let area1 = getArea(10, 20); // 인수
+console.log(area1);
+let area2 = getArea(20, 30); // 인수
+console.log(area2);
+getArea(20, 100); // 인수
+
+//호이스팅 : 선언문을 호출문 아래에 두어도 오류 x
 function getArea(width, height) {
   // 매개 변수
   // let width = 10;
   // let height = 20;
 
   function another() {
+    // 중첩 함수
     console.log("another");
   }
   another();
@@ -22,9 +30,3 @@ function getArea(width, height) {
   // console.log(area);
   return area; // 반환값(함수 결과값)
 }
-
-let area1 = getArea(10, 20); // 인수
-console.log(area1);
-let area2 = getArea(20, 30); // 인수
-console.log(area2);
-getArea(20, 100); // 인수
