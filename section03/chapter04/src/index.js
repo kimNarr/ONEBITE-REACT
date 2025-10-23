e8b9f94f5f1ea64003e9c50701f0a1cea8f2e3b1
dj_kim <ignitionhero@gmail.com>
2025-10-23 17:36:49 +0900

    feat: node js

---
+++ section03/chapter04/src/index.js
@@ -0,0 +1,10 @@
+// const moduleData = require("./math");
+// const { add, sub } = require("./math");
+
+import multiply, { add, sub } from "./math.js";
+
+console.log(add(1, 2));
+console.log(sub(1, 2));
+console.log(multiply(1, 2));
+
+// console.log(moduleData);
---
+++ section03/chapter05/src/index.js
@@ -0,0 +1,6 @@
+import multiply, { add, sub } from "./math.js";
+
+import randomcolor from "randomcolor";
+
+const color = randomcolor();
+console.log(color);
