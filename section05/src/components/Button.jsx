4920a222ebd5daa6dfee738eebcba9647ac9c2d8
dj_kim <ignitionhero@gmail.com>
2025-10-24 16:21:57 +0900

    feat: 이벤트 처리

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,7 +1,18 @@
 const Button = ({ children, text, color = "blue" }) => {
   // console.log(props);
+
+  // 이벤트 객체
+  const onClickButton = (e) => {
+    console.log(e);
+    console.log(text);
+  };
+
   return (
-    <button style={{ color: color }}>
+    <button
+      onClick={onClickButton}
+      // onMouseEnter={onClickButton}
+      style={{ color: color }}
+    >
       {text} - {color.toUpperCase()}
       {children}
     </button>
