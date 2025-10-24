9c9448ae8431ca954b3c8f73021dae7197517e1a
dj_kim <ignitionhero@gmail.com>
2025-10-24 16:36:44 +0900

    feat: State로 상태 관리하기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,30 +1,56 @@
 import "./App.css";
-import Main from "./components/Main";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Button from "./components/Button";
+import { useState } from "react";
+// import Main from "./components/Main";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import Button from "./components/Button";
 
 function App() {
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
+  // const buttonProps = {
+  //   text: "메일",
+  //   color: "red",
+  //   a: 1,
+  //   b: 2,
+  //   c: 3,
+  // };
+
+  const [count, setCount] = useState(0);
+
+  const [light, setLight] = useState("off");
 
   return (
     <>
       {/* <Header />
       <Main />
       <Footer /> */}
-      <Button {...buttonProps} />
+
+      {/* <Button {...buttonProps} />
       <Button text={"카페"}>
         <Header />
       </Button>
       <Button text={"블로그"}>
         <div>자식요소</div>
-      </Button>
+      </Button> */}
+      <div>
+        <h1>{light}</h1>
+        <button
+          onClick={() => {
+            setLight(light === "on" ? "off" : "on");
+          }}
+        >
+          {light === "on" ? "끄기" : "켜기"}
+        </button>
+      </div>
+      <div>
+        <h1>{count}</h1>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
     </>
   );
 }
