62394e67e3fc2f0336c96fad9e2972123e4ac3eb
dj_kim <ignitionhero@gmail.com>
2025-10-27 17:48:59 +0900

    feat: Todo List - 리스트 렌더링 하기(검색기능 추가)

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -8,19 +8,19 @@ const mockData = [
   {
     id: 0,
     isDone: false,
-    constent: "react 공부하기",
+    content: "react 공부하기",
     date: new Date().getTime(),
   },
   {
     id: 1,
     isDone: false,
-    constent: "빨래하기",
+    content: "빨래하기",
     date: new Date().getTime(),
   },
   {
     id: 2,
     isDone: false,
-    constent: "청소하기",
+    content: "청소하기",
     date: new Date().getTime(),
   },
 ];
@@ -45,7 +45,7 @@ function App() {
     <div className="App">
       <Header />
       <Editor onCreate={onCreate} />
-      <List />
+      <List todos={todos} />
     </div>
   );
 }
--- section08/src/component/List.jsx
+++ section08/src/component/List.jsx
@@ -1,15 +1,39 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
+import { useState } from "react";
 
-const List = () => {
+const List = ({ todos }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter(
+      (todo) => todo.content.toLowerCase().includes(search.toLowerCase())
+      // todo 의 content 를 소문자로, search state의 값도 소문자로 통일해서
+      // 대소문자 관계없이 검색가능
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+
   return (
     <div className="List">
       <h4>Todo List👌</h4>
-      <input placeholder="검색어를 입력하세요" />
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
       <div className="TodosWrapper">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {filteredTodos.map((todo) => {
+          return <TodoItem key={todo.id} {...todo} />;
+        })}
       </div>
     </div>
   );
