20967d7db69db57eddadd6318dc64d357e21e8d8
dj_kim <ignitionhero@gmail.com>
2025-10-27 16:49:03 +0900

    feat: Todo List 기능 구현 준비

--- section08/src/App.jsx
+++ section08/src/App.jsx
@@ -4,7 +4,30 @@ import Editor from "./component/Editor";
 import List from "./component/List";
 import "./App.css";
 
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    constent: "react 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    constent: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    constent: "청소하기",
+    date: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(mockData);
+
   return (
     <div className="App">
       <Header />
