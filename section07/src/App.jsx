fd506ac7beff014c56dc893aba37a7b54d0069c3
dj_kim <ignitionhero@gmail.com>
2025-10-27 15:40:07 +0900

    feat: useEffect로 라이프사이클 제어하기

--- section07/src/App.jsx
+++ section07/src/App.jsx
@@ -1,15 +1,36 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Viewer from "./component/Viewer";
 import Controller from "./component/Controller";
+import Even from "./component/Even";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
   const [input, setInput] = useState("");
+  const isMount = useRef(false);
 
+  // 1. 마운트: 탄생
   useEffect(() => {
-    console.log(`count: ${count} / input: ${input}`);
-  }, [count, input]);
+    console.log("mount");
+  }, []);
+
+  // 2. 업데이트: 변화, 리렌더링
+  // deps생략 callback 함수는 마운트 될 때와 리렌더링 될 때 실행됨
+  // update 될 때만 실행 : useRef 사용
+  useEffect(() => {
+    if (!isMount.current) {
+      isMount.current = true;
+      return; // 강제 종료
+    }
+    console.log("update");
+  });
+
+  // 3. 언마운트: 죽음
+  useEffect(() => {});
+
+  // useEffect(() => {
+  //   console.log(`count: ${count} / input: ${input}`);
+  // }, [count, input]);
   // 의존성 배열
   // dependency array
   // deps
@@ -30,6 +51,7 @@ function App() {
       </section>
       <section>
         <Viewer count={count} />
+        {count % 2 === 0 ? <Even /> : null}
       </section>
       <section>
         <Controller onClickButton={onClickButton} />
