128fdd2a2c388a39babb79d9bb116a1f0c6d6e0e
dj_kim <ignitionhero@gmail.com>
2025-10-28 11:34:21 +0900

    feat: Todo List - Reducer 적용

---
+++ section09/src/App.jsx
@@ -0,0 +1,116 @@
+import { useState, useRef, useReducer } from "react";
+import Header from "./component/Header";
+import Editor from "./component/Editor";
+import List from "./component/List";
+// import Exam from "./component/Exam";
+import "./App.css";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "react 공부하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래하기",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "청소하기",
+    date: new Date().getTime(),
+  },
+];
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE":
+      return [action.data, ...state];
+    case "UPDATE":
+      return state.map((item) =>
+        item.id === action.targetId ? { ...item, isDone: !item.idDone } : item
+      );
+    case "DELETE":
+      return state.filter((item) => item.id !== action.targetId);
+    default:
+      return state;
+  }
+}
+
+function App() {
+  // const [todos, setTodos] = useState(mockData);
+  const [todos, dispatch] = useReducer(reducer, mockData);
+
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    // const newTodo = {
+    //   id: idRef.current++,
+    //   isDone: false,
+    //   content: content,
+    //   date: new Date().getTime(),
+    // };
+
+    // setTodos([newTodo, ...todos]);
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        isDone: false,
+        content: content,
+        date: new Date().getTime(),
+      },
+    });
+  };
+
+  const onUpdate = (targetId) => {
+    // todos State 값 중
+    // target ID와 일치하는 id를 갖는 todo 아이템의 isDone 변경
+
+    // 인수 : todos 배열에서 targetID와 일치하는 id를 갖는 요소의 데이터만 딱 바꾼 새로운 배열
+    // setTodos(
+    //   todos.map((todo) => {
+    //     if (todo.id === targetId) {
+    //       return {
+    //         ...todo,
+    //         idDone: !todo.isDone,
+    //       };
+    //     }
+    //     return todo;
+    //   })
+    // );
+    // setTodos(
+    //   todos.map((todo) =>
+    //     todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo
+    //   )
+    // );
+    dispatch({
+      type: "UPDATE",
+      targetId: targetId,
+    });
+  };
+
+  const onDelete = (targetId) => {
+    // 인수 : targetId 와 일치하는 id를 갖는 요소만 삭제한 새로운 배열
+    // setTodos(todos.filter((todo) => todo.id !== targetId));
+    dispatch({
+      type: "DELETE",
+      targetId: targetId,
+    });
+  };
+
+  return (
+    <div className="App">
+      <Header />
+      <Editor onCreate={onCreate} />
+      <List todos={todos} onUpdate={onUpdate} onDelete={onDelete} />
+      {/* <Exam /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ section09/src/component/List.jsx
@@ -0,0 +1,49 @@
+import "./List.css";
+import TodoItem from "./TodoItem";
+import { useState } from "react";
+
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter(
+      (todo) => todo.content.toLowerCase().includes(search.toLowerCase())
+      // todo 의 content 를 소문자로, search state의 값도 소문자로 통일해서
+      // 대소문자 관계없이 검색가능
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+
+  return (
+    <div className="List">
+      <h4>Todo List👌</h4>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="TodosWrapper">
+        {filteredTodos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
