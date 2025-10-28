ccf98ba3c84987533ee5056a88f2cbd8625b3c66
dj_kim <ignitionhero@gmail.com>
2025-10-28 13:35:50 +0900

    feat: useMemo

---
+++ section10/src/component/List.jsx
@@ -0,0 +1,74 @@
+import "./List.css";
+import TodoItem from "./TodoItem";
+import { useState, useMemo } from "react";
+
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter(
+      (todo) => todo.content.toLowerCase().includes(search.toLowerCase())
+      // todo 의 content 를 소문자로, search state의 값도 소문자로 통일해서
+      // 대소문자 관계없이 검색가능
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+
+  // const getAnalyzedData = () => {
+  //   console.log("getAnalyzedData");
+  //   const totalCount = todos.length;
+  //   const doneCount = todos.filter((todo) => todo.isDone).length;
+  //   const notDoneCount = totalCount - doneCount;
+
+  //   return { totalCount, doneCount, notDoneCount };
+  // };
+
+  // const { totalCount, doneCount, notDoneCount } = getAnalyzedData();
+
+  const { totalCount, doneCount, notDoneCount } = useMemo(() => {
+    console.log("getAnalyzedData");
+    const totalCount = todos.length;
+    const doneCount = todos.filter((todo) => todo.isDone).length;
+    const notDoneCount = totalCount - doneCount;
+
+    return { totalCount, doneCount, notDoneCount };
+  }, [todos]);
+
+  return (
+    <div className="List">
+      <h4>Todo List👌</h4>
+      <div>
+        <div>total : {totalCount}</div>
+        <div>done : {doneCount}</div>
+        <div>not done : {notDoneCount}</div>
+      </div>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="TodosWrapper">
+        {filteredTodos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
