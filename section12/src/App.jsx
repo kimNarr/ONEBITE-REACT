98b415bf53e8ec7e32d2aba159f87dbf3f935e1c
dj_kim <ignitionhero@gmail.com>
2025-10-29 10:45:05 +0900

    feat: Font Image 설정

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -4,6 +4,7 @@ import Diary from "./pages/Diary";
 import New from "./pages/New";
 import NotFound from "./pages/NotFound";
 import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { getEmotionImage } from "./util/get-emotion-image";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
@@ -17,6 +18,8 @@ import { Routes, Route, Link, useNavigate } from "react-router-dom";
 // Query String : ? 뒤에 변수명과 값 명시  ~/search?q=검색어
 // 검색어 등의 자주 변경되는 값을 주소로 명시하기 위해 사용
 
+//
+
 const App = () => {
   const nav = useNavigate();
 
@@ -26,6 +29,23 @@ const App = () => {
 
   return (
     <>
+      {/* build 시 vite 에서 제공하는 최적화 X */}
+      {/* <div>
+        <img src="/emotion1.png" alt="매우 좋음" />
+        <img src="/emotion2.png" alt="좋음" />
+        <img src="/emotion3.png" alt="보통" />
+        <img src="/emotion4.png" alt="나쁨" />
+        <img src="/emotion5.png" alt="매우 나쁨" />
+      </div> */}
+      {/* build 시 vite 에서 제공하는 최적화 O / Data URI 로 제공됨 (캐시 이용) */}
+      {/* but 이미지 개수 및 용량이 많을 경우 public 폴더에 저장하는게 유리 할 수 도있음 */}
+      <div>
+        <img src={getEmotionImage(1)} alt="매우 좋음" />
+        <img src={getEmotionImage(2)} alt="좋음" />
+        <img src={getEmotionImage(3)} alt="보통" />
+        <img src={getEmotionImage(4)} alt="나쁨" />
+        <img src={getEmotionImage(5)} alt="매우 나쁨" />
+      </div>
       <div>
         <Link to={"/"}>home</Link>
         <Link to={"/new"}>new</Link>
